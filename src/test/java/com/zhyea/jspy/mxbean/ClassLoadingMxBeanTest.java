97542182e0559d8053fa9502229c33f32ac853fe
rui.zhang <rui.zhang@tendcloud.com>
2017-08-19 22:07:18 +0800

    初始化，完成了gc count

---
+++ src/test/java/com/zhyea/jspy/mxbean/ClassLoadingMxBeanTest.java
@@ -0,0 +1,25 @@
+package com.zhyea.jspy.mxbean;
+
+import junit.framework.TestCase;
+
+import java.lang.management.ClassLoadingMXBean;
+import java.lang.management.ManagementFactory;
+
+public class ClassLoadingMxBeanTest extends TestCase {
+
+    ClassLoadingMXBean mxBean = ManagementFactory.getClassLoadingMXBean();
+
+    public void testInfo(){
+
+        System.out.println(mxBean.getObjectName());
+        System.out.println(mxBean.getLoadedClassCount());
+        System.out.println(mxBean.getUnloadedClassCount());
+        System.out.println(mxBean.getTotalLoadedClassCount());
+
+        System.out.println("-----------------------------------------------");
+
+        mxBean.setVerbose(true);
+        System.out.println(mxBean.getLoadedClassCount());
+    }
+
+}
---
+++ src/test/java/com/zhyea/jspy/mxbean/MemoryManagerMxBeansTest.java
@@ -0,0 +1,23 @@
+package com.zhyea.jspy.mxbean;
+
+import junit.framework.TestCase;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryManagerMXBean;
+import java.util.List;
+
+public class MemoryManagerMxBeansTest extends TestCase {
+
+    private List<MemoryManagerMXBean> mxBeans = ManagementFactory.getMemoryManagerMXBeans();
+
+    public void testMxBeans() {
+        for (MemoryManagerMXBean mmm : mxBeans) {
+            System.out.println(mmm.getName());
+            System.out.println(mmm.getObjectName());
+            System.out.println(mmm.getMemoryPoolNames());
+            System.out.println(mmm.isValid());
+
+            System.out.println("-----------------------------------------------------------------------");
+        }
+    }
+}
