1b27af09500598290092ab9d9f7b89b1ab20d673
zhyea <zhangrui@zhyea.com>
2017-11-25 21:52:48 +0800

    准备计算对象size相关功能

--- jspy-agent/src/main/java/com/zhyea/jspy/agent/JSpyAgent.java
+++ jspy-agent/src/main/java/com/zhyea/jspy/agent/JSpyAgent.java
@@ -2,13 +2,32 @@ package com.zhyea.jspy.agent;
 
 import java.lang.instrument.Instrumentation;
 
+
 public class JSpyAgent {
 
+
+    private static Instrumentation instrumentation;
 
     public static void premain(String options, Instrumentation ins) {
 
         System.out.println("-----options: " + options);
         ins.addTransformer(new JSpyTransformer(options, ins));
+
+        if (null != instrumentation) {
+            throw new AssertionError("instrumentation already initialized.");
+        }
+
+        instrumentation = ins;
+    }
+
+    /**
+     * 获取instrumentation实例，以便计算对象size
+     */
+    public static Instrumentation getInstrumentation() {
+        if (null == instrumentation) {
+            throw new IllegalStateException("Instrumentation is not setup properly.");
+        }
+        return instrumentation;
     }
 
 
