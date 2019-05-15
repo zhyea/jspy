f4666fbfeeb353543c0a9019125a74e0785a6fba
zhyea <zhangrui@zhyea.com>
2019-05-15 23:30:05 +0800

    alter

--- server/src/main/java/org/chobit/jspy/web/HelloController.java
+++ server/src/main/java/org/chobit/jspy/web/HelloController.java
@@ -1,9 +1,17 @@
 package org.chobit.jspy.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/m")
 public class HelloController {
 
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Thanks";
+    }
 
 }
