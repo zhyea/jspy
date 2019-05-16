29908ebbb679cebdb631a8ac8e6b02cfd86d0374
rui.zhang <rui.zhang@tendcloud.com>
2019-05-16 19:22:01 +0800

    alter
    Change-Id: I04272c0c3c1cca7574c12a015c0d8a015c8b0270

--- server/src/main/java/org/chobit/jspy/JSpyApplication.java
+++ server/src/main/java/org/chobit/jspy/JSpyApplication.java
@@ -1,11 +1,9 @@
 package org.chobit.jspy;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-@SpringBootConfiguration
+@SpringBootApplication
 public class JSpyApplication {
 
 
--- server/src/main/java/org/chobit/jspy/web/HelloController.java
+++ server/src/main/java/org/chobit/jspy/web/HelloController.java
@@ -1,11 +1,9 @@
 package org.chobit.jspy.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/m")
 public class HelloController {
 
 
