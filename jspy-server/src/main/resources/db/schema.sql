2f022d0136f269cbaefecd25b3ebeba78d14743a
rui.zhang <rui.zhang@tendcloud.com>
2019-05-20 19:51:18 +0800

    alter
    Change-Id: Ie7f548e4533d90d8e838fb2d3a774301a206c98a

--- jspy-server/src/main/resources/db/schema.sql
+++ jspy-server/src/main/resources/db/schema.sql
@@ -8,4 +8,16 @@ create table user
     deleted     tinyint                     default 0,
     insert_time datetime                    default current_date,
     op_time     timestamp          not null default current_timestamp on update current_timestamp
-) ;
+);
+
+
+create table app
+(
+    id          int identity primary key,
+
+    app_id      varchar(16) unique not null,
+
+    deleted     tinyint                     default 0,
+    insert_time datetime                    default current_date,
+    op_time     timestamp          not null default current_timestamp on update current_timestamp
+)
