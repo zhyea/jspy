60cf5eba6b27ea2d7ef12f6414d0a7ff526b5d44
rui.zhang <rui.zhang@tendcloud.com>
2019-05-20 20:16:18 +0800

    alter
    Change-Id: Ibf4ef341c3f7d44df30278144c80ab547121fc6a

---
+++ jspy-server/src/test/java/org/chobit/jspy/Test.java
@@ -0,0 +1,82 @@
+package org.chobit.jspy;
+
+import java.util.Random;
+
+public class Test {
+
+    private static final char[] r = new char[]{'q', 'w', 'e', '8', 's', '2', 'd', 'z', 'x', '9', 'c', '7', 'p', '5', 'k', '3', 'm', 'j', 'u', 'f', 'r', '4', 'v', 'y', 't', 'n', '6', 'b', 'g', 'h'};
+
+    /**
+     * 定义一个字符用来补全邀请码长度（该字符前面是计算出来的邀请码，后面是用来补全用的）
+     */
+    private static final char b = 'a';
+
+    /**
+     * 进制长度
+     */
+    private static final int binLen = r.length;
+
+    /**
+     * 邀请码长度
+     */
+    private static final int s = 6;
+
+    /**
+     * 根据ID生成随机码
+     *
+     * @param id ID
+     * @return 随机码
+     */
+    public static String toSerialCode(long id) {
+        char[] buf = new char[32];
+        int charPos = 32;
+
+        while ((id / binLen) > 0) {
+            int ind = (int) (id % binLen);
+            buf[--charPos] = r[ind];
+            id /= binLen;
+        }
+
+
+        buf[--charPos] = r[(int) (id % binLen)];
+        String str = new String(buf, charPos, (32 - charPos));
+        // 不够长度的自动随机补全
+        if (str.length() < s) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(b);
+            Random rnd = new Random();
+            for (int i = 1; i < s - str.length(); i++) {
+                sb.append(r[rnd.nextInt(binLen)]);
+            }
+            str += sb.toString();
+        }
+        return str;
+    }
+
+    /**
+     *
+     */
+    public static long codeToId(String code) {
+        char chs[] = code.toCharArray();
+        long res = 0L;
+        for (int i = 0; i < chs.length; i++) {
+            int ind = 0;
+            for (int j = 0; j < binLen; j++) {
+                if (chs[i] == r[j]) {
+                    ind = j;
+                    break;
+                }
+            }
+            if (chs[i] == b) {
+                break;
+            }
+            if (i > 0) {
+                res = res * binLen + ind;
+            } else {
+                res = ind;
+            }
+        }
+        return res;
+    }
+
+}
