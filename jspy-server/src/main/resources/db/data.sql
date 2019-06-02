06d57066b2d3c6aceac1328ca6142d93a0fb6e4b
zhyea <zhangrui@zhyea.com>
2019-06-02 23:26:13 +0800

    in coding

--- jspy-server/src/main/resources/db/data.sql
+++ jspy-server/src/main/resources/db/data.sql
@@ -1,3 +1,7 @@
 insert into user (username, password)
 select 'admin', 'admin' from dual
     where not exists (select 1 from user where username='admin');
+
+insert into app (app_name, app_code)
+select 'JSPY Console', 'jSpyCon' from dual
+    where not exists (select 1 from app where app_code='jSpyCon');
