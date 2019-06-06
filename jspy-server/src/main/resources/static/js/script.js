64915ab0d5b6c423359cb1cec357b1d0389d55f8
rui.zhang <rui.zhang@tendcloud.com>
2019-06-06 19:32:51 +0800

    in coding
    Change-Id: Ic891386c5346a393f4c5d238803bc49fd417664a

--- jspy-server/src/main/resources/static/js/script.js
+++ jspy-server/src/main/resources/static/js/script.js
@@ -1,36 +1,47 @@
 Date.prototype.format = function (fmt) {
 
-    var o = {
-        "M+" : this.getMonth()+1,                 //月份   
-        "d+" : this.getDate(),                    //日   
-        "h+" : this.getHours(),                   //小时   
-        "H+" : this.getHours(),                   //小时   
-        "m+" : this.getMinutes(),                 //分   
-        "s+" : this.getSeconds(),                 //秒   
-        "q+" : Math.floor((this.getMonth()+3)/3), //季度   
-        "S"  : this.getMilliseconds()             //毫秒   
+    let o = {
+        "M+": this.getMonth() + 1,                 //月份   
+        "d+": this.getDate(),                    //日   
+        "h+": this.getHours(),                   //小时   
+        "H+": this.getHours(),                   //小时   
+        "m+": this.getMinutes(),                 //分   
+        "s+": this.getSeconds(),                 //秒   
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度   
+        "S": this.getMilliseconds()             //毫秒   
     };
-    if(/(y+)/.test(fmt)){
-        fmt=fmt.replace(RegExp.$1,(this.getFullYear()+"").substr(4-RegExp.$1.length));
-    };
-    for(var k in o){
-        if(new RegExp("("+ k +")").test(fmt)){
-            fmt=fmt.replace(RegExp.$1,(RegExp.$1.length==1)?(o[k]):(("00"+ o[k]).substr((""+o[k]).length)));
-        };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
     }
-    return fmt;
+
+    for (let k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+    return fmt;
 };
 
 Number.prototype.formatSize = function () {
-    var sizes = ['B','K','M','G','T'];
-    if(0==this) return 0;
-    var i = parseInt(Math.floor(Math.log(this) / Math.log(1024)));
+    let sizes = ['B', 'K', 'M', 'G', 'T'];
+    if (0 == this) return 0;
+    let i = parseInt(Math.floor(Math.log(this) / Math.log(1024)));
     return (this / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
 };
 
-function formatSize(bytes){
-    var sizes = ['B','K','M','G','T'];
+function formatSize(bytes) {
+    let sizes = ['B', 'K', 'M', 'G', 'T'];
     if (bytes == 0) return 0;
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
     return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
-};
+}
+
+
+function copyToClipboard(text) {
+    let aux = document.createElement("input");
+    aux.setAttribute("value", text);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("Copy");
+    document.body.removeChild(aux);
+}
