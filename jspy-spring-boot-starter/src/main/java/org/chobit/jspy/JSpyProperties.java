1fafe1d17df37a583cfd7757d81ce0d58c1fa242
rui.zhang <rui.zhang@tendcloud.com>
2019-06-11 19:33:33 +0800

    in coding
    Change-Id: Ia3c418562f0bdce9bb88f98ccd4d7a4da114cb1d

--- jspy-spring-boot-starter/src/main/java/org/chobit/jspy/JSpyProperties.java
+++ jspy-spring-boot-starter/src/main/java/org/chobit/jspy/JSpyProperties.java
@@ -3,7 +3,53 @@ package org.chobit.jspy;
 public class JSpyProperties {
 
 
+    private String appCode;
 
+    private String serverHost;
 
+    private int serverPort;
 
+    private int maxIdleConnections = 16;
+
+    private int memoryCollectIntervalSeconds = 6;
+
+    public String getAppCode() {
+        return appCode;
+    }
+
+    public void setAppCode(String appCode) {
+        this.appCode = appCode;
+    }
+
+    public String getServerHost() {
+        return serverHost;
+    }
+
+    public void setServerHost(String serverHost) {
+        this.serverHost = serverHost;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public int getMaxIdleConnections() {
+        return maxIdleConnections;
+    }
+
+    public void setMaxIdleConnections(int maxIdleConnections) {
+        this.maxIdleConnections = maxIdleConnections;
+    }
+
+    public int getMemoryCollectIntervalSeconds() {
+        return memoryCollectIntervalSeconds;
+    }
+
+    public void setMemoryCollectIntervalSeconds(int memoryCollectIntervalSeconds) {
+        this.memoryCollectIntervalSeconds = memoryCollectIntervalSeconds;
+    }
 }
