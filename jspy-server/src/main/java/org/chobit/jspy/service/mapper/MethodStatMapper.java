667a85c52bacc6a0bcc2438b62279b7395b7123b
rui.zhang <rui.zhang@tendcloud.com>
2019-07-15 19:51:02 +0800

    前端调整
    Change-Id: Ifb960b44936311f62212aad946ba675b210b3964

--- jspy-server/src/main/java/org/chobit/jspy/service/mapper/MethodStatMapper.java
+++ jspy-server/src/main/java/org/chobit/jspy/service/mapper/MethodStatMapper.java
@@ -17,8 +17,8 @@ public interface MethodStatMapper {
             "insert into method_stat(app_code, ip, method_id, std_dev, min, max, mean, percentile999, percentile98, percentile95, percentile90, percentile75, median)",
             "values",
             "<foreach collection='gcStats' item='item' separator=','>",
-            "(#{appCode}, #{ip}, #{methodId}, #{stdDev}, #{min}, #{max}, #{mean},",
-            " #{percentile999}, #{percentile98}, #{percentile95}, #{percentile90}, #{percentile75}, #{median})",
+            "(#{item.appCode}, #{item.ip}, #{item.methodId}, #{item.stdDev}, #{item.min}, #{item.max}, #{item.mean},",
+            " #{item.percentile999}, #{item.percentile98}, #{item.percentile95}, #{item.percentile90}, #{item.percentile75}, #{item.median})",
             "</foreach>",
             "</script>"
     })
